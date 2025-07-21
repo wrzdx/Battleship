ad9e0c77814ac5bea6736e2d5892a2f1a1e08d6e
wrzdx <7zhhovalyg@mail.ru>
2025-07-22 00:43:12 +0300

    Update placeShip() method to prevent close ship placing

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,5 +1,12 @@
 import Ship from "./ship";
 
+class Cell {
+  constructor() {
+    this.isHit = false;
+    this.ship = null;
+  }
+}
+
 export default class GameBoard {
   constructor(size = 8) {
     this.size = size;
@@ -9,26 +16,59 @@ export default class GameBoard {
 
   init() {
     this.board = Array.from({ length: this.size }, () =>
-      Array(this.size).fill(null)
+      Array(this.size).fill(new Cell())
     );
   }
 
   placeShip(positions) {
-    const possibleToPlace = positions.every(
-      (pos) =>
-        pos[0] >= 0 &&
-        pos[0] < this.board.length &&
-        pos[1] >= 0 &&
-        pos[1] < this.board[0].length &&
-        !this.board[pos[0]][pos[1]]
-    );
+    const surroundingPositions = [];
+    positions.forEach((pos) => {
+      const surrounding = [
+        [pos[0] - 1, pos[1] - 1],
+        [pos[0] - 1, pos[1]],
+        [pos[0] - 1, pos[1] + 1],
+        [pos[0], pos[1] - 1],
+        [pos[0], pos[1] + 1],
+        [pos[0] + 1, pos[1] - 1],
+        [pos[0] + 1, pos[1]],
+        [pos[0] + 1, pos[1] + 1],
+      ];
+      surrounding.forEach((pos) => {
+        if (!surroundingPositions.includes(pos)) {
+          surroundingPositions.push(pos);
+        }
+      });
+    });
+
+    const possibleToPlace =
+      positions.every(
+        (pos) =>
+          pos[0] >= 0 &&
+          pos[0] < this.board.length &&
+          pos[1] >= 0 &&
+          pos[1] < this.board[0].length &&
+          !this.board[pos[0]][pos[1]].isHit &&
+          !this.board[pos[0]][pos[1]].ship
+      ) &&
+      surroundingPositions.every(
+        (pos) =>
+          !(
+            pos[0] >= 0 &&
+            pos[0] < this.board.length &&
+            pos[1] >= 0 &&
+            pos[1] < this.board[0].length
+          ) || !this.board[pos[0]][pos[1]].ship
+      );
+
     if (possibleToPlace) {
       const ship = new Ship(positions.length);
       positions.forEach((pos) => {
-        this.board[pos[0]][pos[1]] = ship;
+        this.board[pos[0]][pos[1]].ship = ship;
       });
     }
 
     return possibleToPlace;
   }
+
+  receiveAttack(position) {}
 }
--- src/gameBoard.test.js
+++ src/gameBoard.test.js
@@ -15,7 +15,8 @@ describe("gameBoard", () => {
     gameBoard.board.forEach((row) => {
       expect(row).toHaveLength(8);
       row.forEach((cell) => {
-        expect(cell).toBeNull()
+        expect(cell.ship).toBeNull();
+        expect(cell.isHit).toBe(false);
       })
     });
   });
@@ -28,18 +29,58 @@ describe("gameBoard", () => {
       const result = gameBoard.placeShip(positions);
 
       expect(result).toBe(true);
-      expect(gameBoard.board[0][0]).not.toBeNull();
-      expect(gameBoard.board[0][1]).not.toBeNull();
+      expect(gameBoard.board[0][0].ship).not.toBeNull();
+      expect(gameBoard.board[0][1].ship).not.toBeNull();
     })
 
-    test("shouldn't place ship in given place on board and return false", () => {
+    test("shouldn't place ship in out of range place on board and return false", () => {
       const positions = [[0, 0], [0, 8]];
 
       const result = gameBoard.placeShip(positions);
 
       expect(result).toBe(false);
-      expect(gameBoard.board[0][0]).toBeNull();
+      expect(gameBoard.board[0][0].ship).toBeNull();
       expect(gameBoard.board[0][8]).toBe(undefined);
     })
+
+    test("shouldn place two ship if the distance between them no less than 1", () => {
+      const shipOne = [[0, 0], [0, 1]];
+      const shipTwo = [[2, 0], [2, 1]];
+      gameBoard.placeShip(shipOne);
+      expect(gameBoard.placeShip(shipTwo)).toBe(true);
+    }) 
+
+    test("shouldn't place ships if the distance between them less than 1", () => {
+      const shipOne = [[0, 0], [0, 1]];
+      const shipTwo = [[1, 0], [1, 1]];
+      const shipThree = [[0, 2], [0, 3]];
+      gameBoard.placeShip(shipOne);
+      expect(gameBoard.placeShip(shipTwo)).toBe(false);
+      expect(gameBoard.placeShip(shipThree)).toBe(false);
+    }) 
   })
+
+  // describe("receiveAttack()", () => {
+  //   beforeEach(() => {
+  //     const positions = [[0, 0], [0, 1]];
+  //     gameBoard.placeShip(positions);
+  //   })
+  //   test("should hit ship on board and return true", () => {
+  //     const result = gameBoard.receiveAttack([0, 0]);
+  //     expect(result).toBe(true);
+  //     expect(gameBoard.board[0][0].hitParts).toContain([0, 0])
+  //   })
+
+  //   test("should hit nothing on board and return false", () => {
+  //     const result = gameBoard.receiveAttack([0, 2]);
+  //     expect(result).toBe(false);
+  //     expect(gameBoard.board[0][2]).toBe(null);
+  //   })
+
+  //   test("should return false if hit the same place twice", () => {
+  //     gameBoard.receiveAttack([0, 0]);
+  //     const result = gameBoard.receiveAttack([0, 0]);
+  //     expect(result).toBe(false);
+  //   })
+  // })
 });
