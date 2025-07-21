09bc98245c5a191c9eb6b4a1561ef56eb3a5485b
wrzdx <7zhhovalyg@mail.ru>
2025-07-21 22:58:51 +0300

    Implement placeShip() method and related tests

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,13 +1,34 @@
+import Ship from "./ship";
+
 export default class GameBoard {
   constructor(size = 8) {
     this.size = size;
     this.board = [];
-    this.init()
+    this.init();
   }
 
   init() {
     this.board = Array.from({ length: this.size }, () =>
       Array(this.size).fill(null)
     );
+  }
+
+  placeShip(positions) {
+    const possibleToPlace = positions.every(
+      (pos) =>
+        pos[0] >= 0 &&
+        pos[0] < this.board.length &&
+        pos[1] >= 0 &&
+        pos[1] < this.board[0].length &&
+        !this.board[pos[0]][pos[1]]
+    );
+    if (possibleToPlace) {
+      const ship = new Ship(positions.length);
+      positions.forEach((pos) => {
+        this.board[pos[0]][pos[1]] = ship;
+      });
+    }
+
+    return possibleToPlace;
   }
 }
--- src/gameBoard.test.js
+++ src/gameBoard.test.js
@@ -1,4 +1,5 @@
 import GameBoard from "./gameBoard";
+import Ship from "./ship";
 
 describe("gameBoard", () => {
   let gameBoard;
@@ -16,7 +17,29 @@ describe("gameBoard", () => {
       row.forEach((cell) => {
         expect(cell).toBeNull()
       })
-
     });
   });
+
+
+  describe("placeShip()", () => {
+    test("should place ship in correct place on board and return true", () => {
+      const positions = [[0, 0], [0, 1]];
+
+      const result = gameBoard.placeShip(positions);
+
+      expect(result).toBe(true);
+      expect(gameBoard.board[0][0]).not.toBeNull();
+      expect(gameBoard.board[0][1]).not.toBeNull();
+    })
+
+    test("shouldn't place ship in given place on board and return false", () => {
+      const positions = [[0, 0], [0, 8]];
+
+      const result = gameBoard.placeShip(positions);
+
+      expect(result).toBe(false);
+      expect(gameBoard.board[0][0]).toBeNull();
+      expect(gameBoard.board[0][8]).toBe(undefined);
+    })
+  })
 });
